4220ac5156948c0e8f1249d1071d3f82305ba1b5
TomLak83 <lakatos.tom@gmail.com>
2021-02-07 17:21:20 +0100

    Appointment entity

---
+++ OptikaRihoskova/Models/Appointment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OptikaRihoskova.Models
+{
+    public class Appointment
+    {
+        public Guid Id { get; set; }
+        public string ClientEmail { get; set; }
+        public string Optometrist { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public int DurationMins { get; set; }
+        public bool OptometristConfiremed { get; set; }
+        public bool ClientConfirmed { get; set; }
+    }
+}
