8207bbde225c7cfebe95d3fd1904e4f38132d669
TomLak83 <lakatos.tom@gmail.com>
2021-02-07 17:27:23 +0100

    Context

---
+++ OptikaRihoskova/DAL/OptometricsContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OptikaRihoskova.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace OptikaRihoskova.DAL
+{
+    public class OptometricsContext : DbContext
+    {
+        public OptometricsContext() : base("OptometricsContext")
+        {
+
+        }
+
+        public DbSet<Appointment> Appointments { get; set; }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
